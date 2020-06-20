5478f2e8918b577a02d6acebdd89db614e7e8566
Evgeny Grebnev <evinlorth@gmail.com>
2020-06-20 16:44:47 +0300

    connect to cloud mongodb

--- app/api_classes/mongo.py
+++ app/api_classes/mongo.py
@@ -3,5 +3,6 @@ from pymongo import MongoClient
 
 class Mongo:
     def __init__(self):
-        client = MongoClient("localhost")
+        client = MongoClient(
+            "mongodb+srv://evgeny:Zh0pAn123@cluster0-g6kar.mongodb.net/herolo_messages?retryWrites=true&w=majority")
         self.db = client.herolo_messages
